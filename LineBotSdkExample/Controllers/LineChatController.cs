8ba73aef7826fd5f0939be2b1d2e8f7b5e42375d
isdaviddong <isdaviddong@hotmail.com>
2017-01-22 20:09:21 +0800

    加入reply sticker, reply image

--- LineBotSdkExample/Controllers/LineChatController.cs
+++ LineBotSdkExample/Controllers/LineChatController.cs
@@ -12,13 +12,17 @@ namespace LineBotSdkExample.Controllers
         [HttpPost]
         public IHttpActionResult POST()
         {
+            //設定你的Channel Access Token
             string ChannelAccessToken = "DETqsnH04xR7Xv...(請輸入你自己的ChannelAccessToken)...1cDnyilFU=";
-
-            //如果有Web.Config app setting
+            isRock.LineBot.Bot bot;
+            //如果有Web.Config app setting，以此優先
             if (System.Configuration.ConfigurationManager.AppSettings.AllKeys.Contains("LineChannelAccessToken"))
             {
                 ChannelAccessToken = System.Configuration.ConfigurationManager.AppSettings["LineChannelAccessToken"];
             }
+
+            //create bot instance
+            bot = new isRock.LineBot.Bot(ChannelAccessToken);
 
             try
             {
@@ -26,11 +30,26 @@ namespace LineBotSdkExample.Controllers
                 string postData = Request.Content.ReadAsStringAsync().Result;
                 //剖析JSON
                 var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
-                //回覆訊息
-                string Message;
-                Message = "哈囉, 你說了:" + ReceivedMessage.events[0].message.text;
-                //回覆用戶
-                isRock.LineBot.Utility.ReplyMessage(ReceivedMessage.events[0].replyToken, Message, ChannelAccessToken);
+                var UserSays = ReceivedMessage.events[0].message.text;
+                var ReplyToken = ReceivedMessage.events[0].replyToken;
+                //依照用戶說的特定關鍵字來回應
+                switch (UserSays.ToLower())
+                {
+                    case "/teststicker":
+                        //回覆貼圖
+                        bot.ReplyMessage(ReplyToken, 1, 1);
+                        break;
+                    case "/testimage":
+                        //回覆圖片
+                        bot.ReplyMessage(ReplyToken, new Uri("https://scontent-tpe1-1.xx.fbcdn.net/v/t31.0-8/15800635_1324407647598805_917901174271992826_o.jpg?oh=2fe14b080454b33be59cdfea8245406d&oe=591D5C94"));
+                        break;
+                    default:
+                        //回覆訊息
+                        string Message="哈囉, 你說了:" + UserSays;
+                        //回覆用戶
+                        bot.ReplyMessage(ReplyToken, Message);
+                        break;
+                }
                 //回覆API OK
                 return Ok();
             }
